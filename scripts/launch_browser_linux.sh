061a2eb81bfe0bf46b6800170b135b22c83ff8e4
Abhishek Roy <royabhishek77@gmail.com>
2025-08-11 11:41:20 +0100

    fix the browser launch command for linux

--- scripts/launch_browser_linux.sh
+++ scripts/launch_browser_linux.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # For development on linux this is required to run webgpu properly
 # otherwise it will run on the CPU. not ideal.
-brave-browser http://127.0.0.1:8080/ \
-  --new-window \
-  --enable-unsafe-webgpu \
-  --enable-features=Vulkan,VulkanFromANGLE,DefaultANGLEVulkan \
-  --use-vulkan
+google-chrome https://abroy77.github.io/game-of-life-wgpu/ --new-window --enable-features=Vulkan --enable-unsafe-webgpu
